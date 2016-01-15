9cd1dca4a279f9de48d30b680981fa7145ec728f
Prakash <prakash@quintype.com>
2016-01-16 00:09:57 +0530

    Adds blocking example

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,4 +1,8 @@
+import ratpack.exec.Blocking
 import ratpack.groovy.template.MarkupTemplateModule
+import ratpack.handling.Context
+
+import java.nio.file.Path
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
@@ -13,6 +17,18 @@ ratpack {
       render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
     }
 
+    get("file"){
+        Path filePath=serverConfig.baseDir.binding("sampleFile.txt").getFile()
+        Blocking.get{
+          filePath.toFile().text
+        }.then{
+          render it
+        }
+      }
+
+    get("better-file"){
+      render serverConfig.baseDir.binding("sampleFile.txt").getFile()
+    }
 
     files { dir "public" }
   }
